ce61722c07be688935f86609120247c07b2fb5b4
Syarif Ubaidillah <ubed56pb@gmail.com>
2026-09-01 21:06:16 +0700

    use scheduling for cronjob support in shared hosting

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,6 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('queue:work --max-time=55')
+    ->everyMinute()
+    ->withoutOverlapping();
